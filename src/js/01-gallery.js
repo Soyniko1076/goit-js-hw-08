1533b9acd6facc3d42689decfe5f9d5db0f6ff9c
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-13 14:38:46 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,16 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-// rendered items
 function createGalleryItemsMarkup(items) {
   return items.map(({ preview, original, description }) => {
     return `
@@ -22,9 +25,8 @@ function createGalleryItemsMarkup(items) {
   }).join('');
 }
 
-// use library SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
 
-// console.log(galleryItems);
+
